4a7b31b5f87c20cd093c5a8bf9d446a9f1a6366a
Audi Armadhani <audiarmadhani@Aulias-MacBook-Air.local>
2025-01-23 11:17:54 +0700

    Resolve error

--- frontend/app/(dashboard)/admin/target/page.js
+++ frontend/app/(dashboard)/admin/target/page.js
@@ -21,8 +21,6 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-
 
 const API_BASE_URL = 'https://processing-facility-backend.onrender.com/api/targets'; // Define your base API URL here
 
