c225026ac84a26c33f0a72788254a51db37d6a86
Audi Armadhani <audiarmadhani@Aulias-MacBook-Air.local>
2025-01-23 11:59:52 +0700

    Change station page

--- frontend/app/(dashboard)/station/page.tsx
+++ frontend/app/(dashboard)/station/page.tsx
@@ -1,13 +1,30 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
-
+import { Link } from '@mui/material'; // Import Link from Material-UI
 
 export default function OrdersPage() {
-  
-
   return (
-    <Typography>
-      Welcome to the Toolpad orders!
-    </Typography>
+    <div>
+      <Typography variant="h5">
+        Nothing to show here really. Maybe go to one of this links?
+      </Typography>
+      <Typography>
+        {/* Create links to the specified routes */}
+        <ul>
+          <li>
+            <Link href="https://kopifabriek-platform.vercel.app/station/receivingstation">Receiving Station</Link>
+          </li>
+          <li>
+            <Link href="https://kopifabriek-platform.vercel.app/station/qcstation">Quality Control Station</Link>
+          </li>
+          <li>
+            <Link href="https://kopifabriek-platform.vercel.app/station/preprocessingstation">Preprocessing Station</Link>
+          </li>
+          <li>
+            <Link href="https://kopifabriek-platform.vercel.app/station/postprocessingstation">Postprocessing Station</Link>
+          </li>
+        </ul>
+      </Typography>
+    </div>
   );
 }
