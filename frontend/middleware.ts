f0b32034899707e6a46bae8a0e508b99000ff927
Audi Armadhani <audiarmadhani@Aulias-MacBook-Air.local>
2025-01-26 08:31:31 +0700

    add sign up page, improve api security

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,7 @@
 export { auth as middleware } from './auth';
 
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|.*\\.png$|auth/signin|auth/signup).*)', // Exclude auth/signin and auth/signup
+  ],
 };
