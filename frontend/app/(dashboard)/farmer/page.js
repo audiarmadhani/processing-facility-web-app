318796fd03cb441a7840f8b1aeb6582365de4c9b
Audi Armadhani <audiarmadhani@Aulias-MacBook-Air.local>
2025-01-28 00:42:32 +0700

    add farmer farm varieities

--- frontend/app/(dashboard)/farmer/page.js
+++ frontend/app/(dashboard)/farmer/page.js
@@ -38,6 +38,17 @@ function FarmerInputStation() {
   const [farmVarieties, setFarmVarieties] = useState([]);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
 
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
   useEffect(() => {
     if (session?.user?.role) {
       fetchFarmerData();
