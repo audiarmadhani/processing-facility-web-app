90c2055c8ee7f1c23fd339951db1af93180e6693
Audi Armadhani <audiarmadhani@Aulias-MacBook-Air.local>
2025-01-29 22:50:10 +0700

    try using mui line chart

--- frontend/app/(dashboard)/charts/ArabicaAvgCostMoM.js
+++ frontend/app/(dashboard)/charts/ArabicaAvgCostMoM.js
@@ -58,10 +58,8 @@ const ArabicaAvgCostMoM = () => {
   }
 
   return (
-    <Box sx={{ width: '100%', height: 200 }}>
+    <Box sx={{ width: '100%', height: '100%' }}>
       <LineChart
-      width={400}
-      height={200}
       series={[
         { data: data.map(item => item.thisMonthCost), label: 'This Month', showMark: false},
         { data: data.map(item => item.lastMonthCost), label: 'Last Month', showMark: false},
