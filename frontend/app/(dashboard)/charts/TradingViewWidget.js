eabb90d3151073577e3093be16190b5b83bea591
Audi Armadhani <audiarmadhani.aa@gmail.com>
2026-04-01 12:54:03 +0700

    dashboard rev

--- frontend/app/(dashboard)/charts/TradingViewWidget.js
+++ frontend/app/(dashboard)/charts/TradingViewWidget.js
@@ -15,22 +15,26 @@ const TradingViewWidget = ({ symbol }) => {
     script.async = true;
 
     script.innerHTML = JSON.stringify({
-      autosize: true,
-      symbol,
-      interval: "60",
-      timezone: "Asia/Jakarta",
-      theme: "light",
-      style: "1",
-      locale: "en",
-      hide_top_toolbar: true,
-      hide_legend: true,
-      allow_symbol_change: false,
+        autosize: true,
+        symbol,
+        interval: "60",
+        timezone: "Asia/Jakarta",
+        theme: "dark",
+        style: "1",
+        locale: "en",
+        hide_top_toolbar: false,
+        hide_legend: false,
+        allow_symbol_change: true,
+        withdateranges: true,
+        details: false,
+        hotlist: false,
+        calendar: false,
     });
 
     containerRef.current.appendChild(script);
   }, [symbol]);
 
-  return <div ref={containerRef} style={{ height: "220px", width: "100%" }} />;
+  return <div ref={containerRef} style={{ height: "380px", width: "100%" }} />;
 };
 
 export default TradingViewWidget;
